4968386dde0a908e897adf54afd0767951992874
Renata Teixeira de Jesus <renata.teixeira@dinfor.uniceub>
2026-03-10 10:23:54 -0300

    estrutura de templates criada

---
+++ src/helpdesk/extensions.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+
+engine = None
+
+def init_db(app):
+    global engine
+
+    db_url = app.config["DATABASE_URL"]
+
+    engine = create_engine(
+        db_url,
+        pool_pre_ping=True,
+        future=True
+    )
